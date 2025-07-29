ea07f345af6454b7b5d4df41d985c8781cebb0ba
maxryan008 <maxryan008@gmail.com>
2025-07-29 20:11:31 +1000

    Initial commit

---
+++ src/client/java/dev/maximus/techcore/client/TechcoreClient.java
@@ -0,0 +1,10 @@
+package dev.maximus.techcore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechcoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/maximus/techcore/Techcore.java
@@ -0,0 +1,10 @@
+package dev.maximus.techcore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Techcore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
